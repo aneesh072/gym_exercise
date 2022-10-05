42973c585918b20b9560cd4f38da3b4c106c1244
aneesh072 <aneeshkarki292@gmail.com>
2022-10-05 20:58:46 +1100

    mapping data of search result on main page

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -19,12 +19,20 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         cursor: 'pointer',
         gap: '47px',
       }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1000, left: 100, behavior: 'smooth' });
+      }}
     >
-      <img
-        src={Icon}
-        alt="dumbell"
-        styles={{ width: '40px', height: '40px' }}
-      />
+      <img src={Icon} alt="dumbell" style={{ width: '40px', height: '40px' }} />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#311212"
+        textTransform="captalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
